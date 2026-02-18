79f7a2d13eab0bc52e0cd2a71121b2b185b09484
Gayathri Mahadevan <gayat_19@yahoo.com>
2026-02-18 09:39:25 +0530

    Task

---
+++ Day14/AsyncSolution/TheardingExampleApp/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace ThreadingExampleApp
+{
+    internal class Program
+    {
+
+         void PrintNumbers()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                
+                Console.WriteLine(i+" "+Thread.CurrentThread.Name);
+                Thread.Sleep(500);
+            }
+            lock(this)
+            {
+                for (int i = 10; i < 100; i = i + 10)
+                {
+                    Console.WriteLine(i + " " + Thread.CurrentThread.Name);
+                    Thread.Sleep(500);
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Program p = new Program();
+            Thread t1 = new Thread(p.PrintNumbers);
+            Thread t2 = new Thread(p.PrintNumbers);
+            t1.Name = "Thread 1";
+            t2.Name = "Thread 2";
+            t1.Start();
+            t2.Start();
+            Console.WriteLine("After the thread start");
+        }
+    }
+}
---
+++ Day14/AsyncSolution/TheardingExampleApp/TaskClass.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheardingExampleApp
+{
+    internal class TaskClass
+    {
+        async Task<int> CalculateSum(int n)
+        {
+            int sum = 0;
+            for (int i = 0; i <= n; i++)
+            {
+                sum += i;
+            }
+            return sum;
+        }
+        public async static Task Main(string[] args)
+        {
+            
+            TaskClass tc = new TaskClass();
+            //Task<int> result = tc.CalculateSum(100);
+            //Console.WriteLine(result.Result);
+            int result = await tc.CalculateSum(100);
+            Console.WriteLine(result);
+        }
+    }
+}
