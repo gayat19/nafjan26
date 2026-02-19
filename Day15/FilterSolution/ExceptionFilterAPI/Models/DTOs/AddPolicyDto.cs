59a20fc078819fba2a047c1a2f2cce71fbb77f22
Gayathri Mahadevan <gayat_19@yahoo.com>
2026-02-19 09:42:26 +0530

    Exception Filter

---
+++ Day15/FilterSolution/ExceptionFilterAPI/Models/DTOs/AddPolicyDto.cs
@@ -0,0 +1,9 @@
+﻿namespace ExceptionFilterAPI.Models.DTOs
+{
+    public class AddPolicyDto
+    {
+        public int InsuranceNumber { get; set; }
+        public int? CustomerId { get; set; }
+        public AddCustomerDto? MyProperty { get; set; }
+    }
+}
