64ec0cd9a9de6bc74294bf13140c8018bb5e4bd0
Gayathri Mahadevan <gayat_19@yahoo.com>
2026-03-13 10:16:55 +0530

    Custom Directive validation

--- Day23/clinicApp/src/app/carddirective.ts
+++ Day23/clinicApp/src/app/carddirective.ts
@@ -6,11 +6,22 @@ import { Directive, ElementRef, HostListener } from '@angular/core';
 export class Carddirective {
 @HostListener('input', ['$event'])
 onBlur(event: Event) {
-  console.log('Input event detected on card input field:', event);
+  var inputElement = event.target as HTMLInputElement;
+  
+  if(inputElement.value.length === 16) {
+    inputElement.style.border = '4px solid green';
+    let formattedValue = '';
+    for(let i = 0; i < inputElement.value.length; i=i+4) {
+      formattedValue += inputElement.value.substring(i, i+4) + ' ';
+    }
+    inputElement.value = formattedValue.trim();
+  } else {
+    inputElement.style.border = '4px solid red';
+  }
 }
 
-  constructor(private elementRef: ElementRef<HTMLInputElement>) {
-    console.log('Card directive initialized for element:');
-    elementRef.nativeElement.style.border = '4px solid blue';
+  constructor() {
+    // console.log('Card directive initialized for element:');
+    // elementRef.nativeElement.style.border = '4px solid blue';
     }
 }
